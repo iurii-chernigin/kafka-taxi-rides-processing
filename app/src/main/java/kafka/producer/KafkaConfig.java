ec7f257e0b722077da91a4135087614ffb3ed550
iurii-chernigin <chernigin.y@gmail.com>
2023-03-15 22:11:00 +0300

    avro_producer: extend and refactor the KafkaConfig class

--- app/src/main/java/kafka/producer/KafkaConfig.java
+++ app/src/main/java/kafka/producer/KafkaConfig.java
@@ -1,5 +1,7 @@
 package kafka.producer;
 
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.streams.StreamsConfig;
@@ -9,6 +11,7 @@ public class KafkaConfig {
  
     private static Properties props = new Properties();
     private static final String bootstrapServer = "pkc-3w22w.us-central1.gcp.confluent.cloud:9092";
+    private static final String schemaRegistryEndpoint = "https://psrc-35wr2.us-central1.gcp.confluent.cloud";
     private static String jaasConfig = String.format(
         "org.apache.kafka.common.security.plain.PlainLoginModule required username='%s' password='%s';",
         Secrets.KAFKA_CLUSTER_KEY, 
@@ -16,14 +19,23 @@ public class KafkaConfig {
     );
   
 
-    public static Properties getProducerConfig() {
-        
+    private static Properties getBasicConfigs() {
+
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.put("security.protocol", "SASL_SSL");
         props.put("sasl.jaas.config", jaasConfig);
         props.put("sasl.mechanism", "PLAIN");
         props.put("client.dns.lookup", "use_all_dns_ips");
         props.put("session.timeout.ms", "45000");
+
+        return props;
+    }
+
+
+    public static Properties getProducerConfig() {
+        
+        props = getBasicConfigs();
+
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
@@ -31,17 +43,25 @@ public class KafkaConfig {
         return props;
     }
 
+
+    public static Properties getAvroProducerConfig() {
+
+        props = getBasicConfigs();
+
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryEndpoint);
+        props.put("basic.auth.credentials.source", "USER_INFO");
+        props.put("basic.auth.user.info", Secrets.SCHEMA_REGISTRY_KEY+":"+Secrets.SCHEMA_REGISTRY_SECRET);
+
+        return props;
+    }
+
     
-    public static Properties getStreamingConfig() {
+    public static Properties getStreamingConfig(String applicationID) {
 
-        final String applicationID = "kafka_streaming.rides_location_counters.v3";
-
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        props.put("security.protocol", "SASL_SSL");
-        props.put("sasl.jaas.config", jaasConfig);
-        props.put("sasl.mechanism", "PLAIN");
-        props.put("client.dns.lookup", "use_all_dns_ips");
-        props.put("session.timeout.ms", "45000");
+        props = getBasicConfigs();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationID);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
